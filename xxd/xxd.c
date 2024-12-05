1e4c76c3bc09d2863a235d2a131b5be20abf2968
Krod-9 <KerrodMcNeal2@gmail.com>
2024-12-04 20:45:06 -0500

    Converts String Into Hexdump

--- xxd/xxd.c
+++ xxd/xxd.c
@@ -1,15 +1,43 @@
 #include <stdio.h>
 
 int main() {
-    // First convert input into ASCII
     char line[100];
     printf("Enter A String Below: ");
     fgets(line, 100, stdin);
     int i = 0;
     while(line[i] != '\0' && line[i] != '\n') {
-        (int)line[i]; 
-        printf("%d ", line[i]);
+        int ascii = (int)line[i];
+        int whole = ascii / 16;
+        int remainder = ascii % 16;
+        if (remainder <= 9) {
+            printf("%d%d ", whole, remainder);
+        } else {
+            switch(remainder) {
+                case 10: 
+                    printf("%dA ", whole);
+                    break;
+                case 11: 
+                    printf("%dB ", whole);
+                    break;
+                case 12: 
+                    printf("%dC ", whole);
+                    break;
+                case 13:
+                    printf("%dD ", whole);
+                    break;
+                case 14:
+                    printf("%dE ", whole);
+                    break;
+                case 15:
+                    printf("%dF ", whole);
+                    break;
+                default:
+                    printf("Error Occured, Remainder Could Not Be Converted To Hexadecimal.");
+                    return 1;
+            }
+        }
         i++;
     }
     return 0;
 }
+ 
